b5aa25a7e8980a5fcac8e9cdcc5a3831322d4755
riannahlol <riannah.smith@carvercollegiate.org>
2025-03-24 18:46:57 +0000

    bouncing box

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,22 +27,29 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
+    createPlatform(195,695,75,30);
+    createPlatform(400,650,75,20);
+    createPlatform(592,590,75,20);
+    createPlatform(805,540,75,30);
+    createPlatform(950,710,75,20);
+    createPlatform(1150,710,75,20);
+    createPlatform(1050,500,75,20);
+    createPlatform(1250,540,75,30);
 
     // TODO 3 - Create Collectables
-
-
+    createCollectable("database",410,630)
+    createCollectable("database",610,575)
+    createCollectable("database",1075,480)
 
     
     // TODO 4 - Create Cannons
-
-
+    createCannon("top",1040,450)
+    createCannon("top",1230,450)
+    createCannon("top", 1350,450)
     
     
     //////////////////////////////////
